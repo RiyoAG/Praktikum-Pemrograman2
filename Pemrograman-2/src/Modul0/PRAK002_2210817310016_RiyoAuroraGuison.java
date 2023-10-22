cc47ca5b1c43e87dc894763d475970f27faa8b1b
RiyoAG <115161952+RiyoAG@users.noreply.github.com>
2023-10-22 20:17:44 +0800

    Add files via upload

---
+++ Pemrograman-2/src/Modul0/PRAK002_2210817310016_RiyoAuroraGuison.java
@@ -0,0 +1,12 @@
+package Modul0;
+
+public class PRAK002_2210817310016_RiyoAuroraGuison {
+    public static void main(String[] args) {
+        System.out.println("##############################");
+        System.out.println("#                            #");
+        System.out.println("#     Riyo Aurora Guison     #");
+        System.out.println("#       2210817310016        #");
+        System.out.println("#                            #");
+        System.out.println("##############################");
+    }
+}
