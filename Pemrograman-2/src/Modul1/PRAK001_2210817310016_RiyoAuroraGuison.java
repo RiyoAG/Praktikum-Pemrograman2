4002c6bc4e700e2e5c5cbad8a863a4e289153b87
RiyoAG <115161952+RiyoAG@users.noreply.github.com>
2023-10-22 20:13:57 +0800

    Delete Pemrograman-2/src/Modul1 directory

--- Pemrograman-2/src/Modul1/PRAK001_2210817310016_RiyoAuroraGuison.java
+++
@@ -1,7 +0,0 @@
-package Modul1;
-
-public class PRAK001_2210817310016_RiyoAuroraGuison {
-	public static void main(String[] args) {
-        System.out.println("Selamat Pagi, Riyo Aurora Guison\nSelamat Siang, Riyo Aurora Guison\nSelamat Malam, Riyo Aurora Guison");
-    }
-}
--- Pemrograman-2/src/Modul1/PRAK004_2210817310016_RiyoAuroraGuison.java
+++
@@ -1,9 +0,0 @@
-package Modul1;
-
-public class PRAK004_2210817310016_RiyoAuroraGuison {
-    public static void main(String[] args) {
-        String nama = ("Riyo Aurora Guison");
-        int umur = 20;
-        System.out.println("Perkenalkan, Nama Saya " + nama + ", Umur Saya " + umur + " Tahun");
-    }
-}
