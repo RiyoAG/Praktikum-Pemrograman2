2e52449aee6e9623ba899f90374fbe51d4d47154
RiyoAG <115161952+RiyoAG@users.noreply.github.com>
2023-11-28 20:32:11 +0800

    Add files via upload

---
+++ Pemrograman-2/src/Modul5/praktikum5/soal1/Shape.java
@@ -0,0 +1,12 @@
+package praktikum5.soal1;
+
+public abstract class Shape {
+    private String shapeName;
+    public Shape(String name){
+        shapeName = name;
+    }
+    public abstract double area();
+    public String toString(){
+        return shapeName;
+    }   
+}
